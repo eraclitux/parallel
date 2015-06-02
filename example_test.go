32b0590efdc9788267f2aa8c984e0ad22fd7d3a3
Andrea Masi <eraclitux@gmail.com>
2015-06-02 15:59:01 +0200

    Simplify Example()

--- example_test.go
+++ example_test.go
@@ -7,9 +7,7 @@ package goparallel_test
 import (
 	"fmt"
 	"math"
-	"os"
 	"runtime"
-	"time"
 
 	"github.com/eraclitux/goparallel"
 )
@@ -43,17 +41,13 @@ func isPrime(n uint64) bool {
 	return true
 }
 
-// FIXME remove time comparison here and use testing's test/benchmark.
-
 // Example shows example usage of the package.
-// It compares time consuption using single core.
 func Example() {
 	// Creates the slice of tasks that we want to execute in parallel.
 	tasks := make([]goparallel.Tasker, 0, 1e3)
 	prev := 1
 	// Limit is the bigger number to check.
 	var limit int = 1e5
-	pre := time.Now()
 	// Create as much tasks as number of cores.
 	d := limit / runtime.NumCPU()
 	for i := 1; i < limit; i++ {
@@ -73,28 +67,11 @@ func Example() {
 	tasks = append(tasks, goparallel.Tasker(j))
 
 	// Run tasks in parallel using all cores.
-	goparallel.RunBlocking(tasks)
-
-	after := time.Now()
-	Δt1 := after.Sub(pre)
-
-	// Lets compare execution time using single core.
-	pre = time.Now()
-	results := make(map[int]bool)
-	for i := 1; i <= limit; i++ {
-		results[i] = isPrime(uint64(i))
+	err := goparallel.RunBlocking(tasks)
+	if err == nil {
+		fmt.Println("Example ok")
 	}
-	after = time.Now()
-	Δt2 := after.Sub(pre)
-	if Δt2 > Δt1 {
-		fmt.Println("Using goparallel takes less time.")
-	} else {
-		fmt.Println("Using goparallel takes more time.")
-	}
-	// We use stderr as stdout is checked to pass test.
-	fmt.Fprintf(os.Stderr, "%30s %9dns\n", "Time with goworker:", Δt1)
-	fmt.Fprintf(os.Stderr, "%30s %9dns\n", "Time without goworker:", Δt2)
 
 	// Output:
-	// Using goparallel takes less time.
+	// Example ok
 }
--- goparallel_test.go
+++ goparallel_test.go
@@ -7,10 +7,17 @@ package goparallel
 import (
 	"fmt"
 	"math"
+	"os"
+	"runtime"
 	"testing"
+	"time"
 )
 
 type dummy struct {
+	done bool
+}
+
+type dummyNop struct {
 	done bool
 }
 
@@ -38,6 +45,13 @@ func (d *dummy) Execute() {
 	d.done = true
 }
 
+func (d dummyNop) Execute() {
+	for i := 0; i < 1e4; i++ {
+		isPrime(uint64(i))
+	}
+	d.done = true
+}
+
 func TestRunBlocking(t *testing.T) {
 	tasks := make([]Tasker, 1e2)
 	// []*dummy does not convert []Tasker.
@@ -59,6 +73,89 @@ func TestRunBlocking(t *testing.T) {
 	}
 }
 
-// TODO test what happen ia a receiver of Execute() in a concrete type.
-// Is it modified into []Tasker?
-//func TestRunBlocking_nopointer(t *testing.T) {}
+// TestRunBlocking_nopointer shows that Execute() method
+// must be implemented on a pointer receiver or computed values
+// will be lost.
+func TestRunBlocking_nopointer(t *testing.T) {
+	tasks := make([]Tasker, 1e1)
+	for i := range tasks {
+		tasks[i] = Tasker(&dummyNop{false})
+	}
+
+	err := RunBlocking(tasks)
+	if err != nil {
+		t.Errorf("Test has failed")
+	}
+	for _, e := range tasks {
+		if e.(*dummyNop).done {
+			fmt.Println("Error, receiver modified!")
+			t.FailNow()
+		}
+	}
+}
+
+type job struct {
+	start   int
+	stop    int
+	results map[int]bool
+}
+
+func (j *job) Execute() {
+	j.results = make(map[int]bool)
+	for i := j.start; i <= j.stop; i++ {
+		j.results[i] = isPrime(uint64(i))
+	}
+}
+
+// Example_performance verify that using goparallel is faster than a serial execution.
+func Example_performance() {
+	// Creates the slice of tasks that we want to execute in parallel.
+	tasks := make([]Tasker, 0, 1e3)
+	prev := 1
+	// Limit is the bigger number to check.
+	var limit int = 1e5
+	pre := time.Now()
+	// Create as much tasks as number of cores.
+	d := limit / runtime.NumCPU()
+	for i := 1; i < limit; i++ {
+		// This is not the best way to disbrubute load
+		// as complexity is not the same in different
+		// intervals (bigger numbers are more difficult to verify),
+		// so some cores remains idle sooner.
+		// We could increase efficency making different interval lenghts.
+		if (i % d) == 0 {
+			j := &job{start: prev, stop: i}
+			prev = i + 1
+			tasks = append(tasks, Tasker(j))
+		}
+	}
+	// Do not forget last interval.
+	j := &job{start: prev, stop: limit}
+	tasks = append(tasks, Tasker(j))
+
+	// Run tasks in parallel using all cores.
+	RunBlocking(tasks)
+
+	after := time.Now()
+	Δt1 := after.Sub(pre)
+
+	// Lets compare execution time using single core.
+	pre = time.Now()
+	results := make(map[int]bool)
+	for i := 1; i <= limit; i++ {
+		results[i] = isPrime(uint64(i))
+	}
+	after = time.Now()
+	Δt2 := after.Sub(pre)
+	if Δt2 > Δt1 {
+		fmt.Println("Using goparallel takes less time.")
+	} else {
+		fmt.Println("Using goparallel takes more time.")
+	}
+	// We use stderr as stdout is checked to pass test.
+	fmt.Fprintf(os.Stderr, "%30s %9dns\n", "Time with goworker:", Δt1)
+	fmt.Fprintf(os.Stderr, "%30s %9dns\n", "Time without goworker:", Δt2)
+
+	// Output:
+	// Using goparallel takes less time.
+}
